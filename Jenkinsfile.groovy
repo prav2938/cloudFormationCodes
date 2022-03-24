c616e5ebef46a92e3aa4c8e90b3ed551f1293135
prav2938 <60060578+prav2938@users.noreply.github.com>
2022-03-24 12:13:31 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,1 @@
-pipeline {
-    agent any
-    stages {
-        stage('Submit Stack') {
-            steps {
-            sh "aws cloudformation create-stack --stack-name s3bucket --template-body file://simplests3cft.json --region 'us-east-1'"
-              }
-             }
-            }
-            }
+println "Ravi"
